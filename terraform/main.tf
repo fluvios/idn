5c5278c6a4fdb67c1201975549756ba3baa65bc5
fluvios <noorfachrian@gmail.com>
2025-01-19 16:08:59 +0700

    fix code

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,25 +92,6 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  }
-
-  zone_id = data.aws_route53_zone.zone.zone_id
-
-  name  = each.value.name
-  type  = each.value.type
-  ttl   = 60
-  records = [
-    each.value.value,
-  ]
-}
-
 # Route 53 Record for the Subdomain
 resource "aws_route53_record" "subdomain" {
   zone_id = data.aws_route53_zone.zone.zone_id
