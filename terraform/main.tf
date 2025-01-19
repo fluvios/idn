7e7dbd8da887bd4731ff11f8519cae9d6b8fe103
fluvios <noorfachrian@gmail.com>
2025-01-19 17:07:12 +0700

    change major code

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,52 @@ data "aws_s3_bucket" "asset_bucket" {
 # Create a new S3 bucket for the main static site
 resource "aws_s3_bucket" "default_site" {
   bucket = "idn-new-timmy-8"
+  force_destroy = true
   tags = {
     Name = "DefaultSiteBucket"
   }
+}
+
+# Add website configuration to the S3 bucket
+resource "aws_s3_bucket_website_configuration" "default_site" {
+  bucket = aws_s3_bucket.default_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Configure public access block for the bucket
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket                  = aws_s3_bucket.default_site.id
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+# Add a bucket policy to allow CloudFront access
+resource "aws_s3_bucket_policy" "default_site_policy" {
+  bucket = aws_s3_bucket.default_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai.id}"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.default_site.id}/*"
+      }
+    ]
+  })
 }
 
 # CloudFront Distribution
@@ -75,6 +118,9 @@ resource "aws_cloudfront_distribution" "cdn" {
     }
   }
 
+  # Specify custom domain in aliases
+  aliases = ["new-timmy-8.serverless.my.id"]  
+
   # SSL settings for HTTPS
   viewer_certificate {
     acm_certificate_arn = "arn:aws:acm:us-east-1:166190020492:certificate/1119d63b-db83-4afb-b726-4a8944f6ec7f"
@@ -101,7 +147,7 @@ resource "aws_route53_record" "subdomain" {
   alias {
     name                   = aws_cloudfront_distribution.cdn.domain_name
     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
-    evaluate_target_health = false
+    evaluate_target_health = true
   }
 }
 
